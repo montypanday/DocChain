04de5143a57170718e3a5266bddebb786571fb0d
pgmitche <pgmitche@deakin.edu.au>
2017-11-14 23:58:48 +1100

    Login tutorial
    Adding of login tutorial page

---
+++ ClientApp/components/tutorial1.tsx
@@ -0,0 +1,24 @@
+﻿import * as React from 'react';
+const img = require('../css/img/LoginBTN.png');
+
+export class Tutorial1 extends React.Component<{}, {}> {
+    public render() {
+        return <div className="body">
+            <div className="center-fade">
+                <p className="welcome">Logging In</p>
+            </div>
+            <div className="pic-container">
+                <section className="login-picture" style={{ backgroundImage: "url(" + img + ")" }}></section>
+            </div>
+            <div className="tutorial-container">
+                <div className="tutorial-start">
+                    <p>
+                        Login to your cloud storage service by navigating to the login page here.
+                    </p>
+                </div>               
+            </div>
+            <a className="button prev" href="/Home/">Return</a>
+            <a className="button next" href="/tutorial2/">Next</a>
+        </div>;
+    }
+}
