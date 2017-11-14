bef2e8f4923fb33500ba213d2a0c86b000223073
pgmitche <pgmitche@deakin.edu.au>
2017-11-14 23:54:29 +1100

    Import of two tutorial pages
    Logging in tutorial, and the File Explorer tutorial. Pictures currently do not render.

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -7,12 +7,16 @@ import { fileExplorer } from './components/fileExplorer';
 import { Login } from './components/login';
 import { Logout } from './components/logout';
 import { contentChecker } from './components/contentChecker';
+import { Tutorial1 } from './components/tutorial1';
+import { Tutorial2 } from './components/tutorial2';
 
 export const routes = <Layout>
     <Route exact path='/' component={Home} />
     <Route path='/fileexplorer' component={fileExplorer} />
     <Route path='/contentpicker' component={contentPicker} />
     <Route path='/contentChecker' component={contentChecker} />
+    <Route path='/tutorial1' component={Tutorial1} />
+    <Route path='/tutorial2' component={Tutorial2} />
     <Route path='/login/:state?/:code?' component={(props) => <Login {...props} code="haha" />} />
     <Route path='/logout' component={Logout} />
 </Layout>;
