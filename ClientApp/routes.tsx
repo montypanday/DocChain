bea756cd75b3816d2393a19928c89310a2f0257c
pgmitche <pgmitche@deakin.edu.au>
2017-11-15 16:34:34 +1100

    Added routing for carousel slides
    Added the routing for the remaining tutorial components

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -9,6 +9,9 @@ import { Logout } from './components/logout';
 import { contentChecker } from './components/contentChecker';
 import { Tutorial1 } from './components/tutorial1';
 import { Tutorial2 } from './components/tutorial2';
+import { Tutorial3 } from './components/tutorial3';
+import { Tutorial4 } from './components/tutorial4';
+import { Tutorial5 } from './components/tutorial5';
 
 export const routes = <Layout>
     <Route exact path='/' component={Home} />
@@ -17,6 +20,9 @@ export const routes = <Layout>
     <Route path='/contentChecker' component={contentChecker} />
     <Route path='/tutorial1' component={Tutorial1} />
     <Route path='/tutorial2' component={Tutorial2} />
+    <Route path='/tutorial3' component={Tutorial3} />
+    <Route path='/tutorial4' component={Tutorial4} />
+    <Route path='/tutorial5' component={Tutorial5} />
     <Route path='/login/:state?/:code?' component={(props) => <Login {...props} code="haha" />} />
     <Route path='/logout' component={Logout} />
 </Layout>;
