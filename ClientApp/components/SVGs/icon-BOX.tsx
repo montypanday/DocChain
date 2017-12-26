ac7e2d20820f71801d6928c9d085654084054d41
pgmitche <pgmitche@deakin.edu.au>
2017-12-27 03:10:55 +1100

    Created dummy icon components
    these will be SVGs for the other providers

---
+++ ClientApp/components/SVGs/icon-BOX.tsx
@@ -0,0 +1,12 @@
+﻿import * as React from "react";
+
+export class BOXIcon extends React.Component {
+
+    public render() {
+        return <svg width="2em" height="2em" viewBox="0 0 1080 1080">
+            <path fill="rgb(255, 193, 7)" d="M362.667 128h298.667l298.667 512h-298.667z" />
+            <path fill="rgb(25, 118, 210)" d="M210.667 896l150.667-256h598.667l-149.333 256z" />
+            <path fill="rgb(76, 175, 80)" d="M64 642.667l146.667 253.333 301.333-512-149.333-256z" />
+        </svg>
+    }
+}
