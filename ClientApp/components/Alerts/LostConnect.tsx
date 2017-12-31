25214f79a95fa4468142511204ee610821a4a491
pgmitche <pgmitche@deakin.edu.au>
2017-12-31 15:21:00 +1100

    Lost Connection component prepared

---
+++ ClientApp/components/Alerts/LostConnect.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from "react";
+import { LostConnIcon } from '../SVGs/icon-LostConn';
+require('./ErrorSplash.css');
+
+export default class LostConnect extends React.Component {
+
+    public render() {
+        return <div className="splash">
+            <LostConnIcon />
+            <p>Connection has been lost. </p>
+        </div>
+    }
+}
