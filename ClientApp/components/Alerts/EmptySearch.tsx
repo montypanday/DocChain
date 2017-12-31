6a939c0caff23255051131cbb34cf1e5f96bc434
pgmitche <pgmitche@deakin.edu.au>
2017-12-31 14:00:52 +1100

    preparing empty search result component

---
+++ ClientApp/components/Alerts/EmptySearch.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from "react";
+import { EmptySearchIcon } from '../SVGs/icon-EmptySearch';
+require('./ErrorSplash.css');
+
+export default class EmptySearch extends React.Component {
+
+    public render() {
+        return <div className="splash">
+            <EmptySearchIcon />
+            <p>Your search returned no results.</p>
+        </div>
+    }
+}
