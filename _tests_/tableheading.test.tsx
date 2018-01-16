f1396aff0ec33fb4d4d9f1655f27c54ebf43b713
LGNRoy <lougnroy@gmail.com>
2018-01-16 13:42:57 +1100

    refresh the snap of row and add a new Jest

---
+++ _tests_/tableheading.test.tsx
@@ -0,0 +1,12 @@
+﻿import * as React from "react";
+import { TableHeading } from "../ClientApp/components/Table";
+import * as renderer from "react-test-renderer";
+
+test("Link changes the class when hovered", () => {
+
+    const component = renderer.create(
+        <TableHeading></TableHeading>,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
